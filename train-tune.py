b25f0429ab25ba695ed2f0ac4169f2c4b0d5685b
Erik <eekihenriksson@gmail.com>
2023-11-08 20:42:30 +0200

    Update train-tune.py

--- train-tune.py
+++ train-tune.py
@@ -1,3 +1,11 @@
+import os
+
+os.environ["TRANSFORMERS_CACHE"] = ".hf/transformers_cache"
+os.environ["HF_HOME"] = ".hf/hf_home"
+os.environ["XDG_CACHE_HOME"] = ".hf/xdg_cache_home"
+os.environ["WANDB_PROJECT"] = "register-labeling"  # log to your project
+os.environ["WANDB_LOG_MODEL"] = "all"  # log your models
+
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import logging
 import sys
