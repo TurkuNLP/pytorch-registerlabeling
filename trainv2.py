510d1c059219e959902b33c9b51b3ca3d87c4835
Erik <eekihenriksson@gmail.com>
2023-11-14 11:57:21 +0200

    Update trainv2.py

--- trainv2.py
+++ trainv2.py
@@ -321,7 +321,8 @@ dataset = load_dataset(
 if options.data_fraction < 1:
     print(f"Using {options.data_fraction*100}% of data")
     for x in ["train", "test", "dev"]:
-        dataset[x] = dataset[x][: options.data_fraction * len(dataset[x])]
+        print(options.data_fraction * len(dataset[x]))
+        dataset[x] = dataset[x][: (options.data_fraction * len(dataset[x]))]
 
 dataset = dataset.shuffle(seed=options.seed)
 
