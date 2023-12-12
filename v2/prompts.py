ffa3911c5138c9f2da492ce96cf9b2e16c70f73d
Erik <eekihenriksson@gmail.com>
2023-12-12 15:32:00 +0200

    Update prompts.py

--- v2/prompts.py
+++ v2/prompts.py
@@ -4,6 +4,27 @@ from .mistral_prompt import prompt
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+from peft import PeftModel
+
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import (
+    FullOptimStateDictConfig,
+    FullStateDictConfig,
+)
+
+import transformers
+from datetime import datetime
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(
+        offload_to_cpu=True, rank0_only=False
+    ),
+)
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+
 
 def run():
     dataset = get_dataset("en-fi-fr-sv", "en-fi-fr-sv", "all")
@@ -16,7 +37,7 @@ def run():
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
     model = AutoModelForCausalLM.from_pretrained(
-        base_model_id, quantization_config=bnb_config
+        base_model_id, quantization_config=bnb_config, cache_dir="cache"
     )
 
     tokenizer = AutoTokenizer.from_pretrained(
@@ -48,6 +69,116 @@ def run():
         print(
             tokenizer.decode(
                 model.generate(**model_input, max_new_tokens=256, pad_token_id=2)[0],
+                skip_special_tokens=True,
+            )
+        )
+
+    model.gradient_checkpointing_enable()
+    model = prepare_model_for_kbit_training(model)
+
+    def print_trainable_parameters(model):
+        """
+        Prints the number of trainable parameters in the model.
+        """
+        trainable_params = 0
+        all_param = 0
+        for _, param in model.named_parameters():
+            all_param += param.numel()
+            if param.requires_grad:
+                trainable_params += param.numel()
+        print(
+            f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+        )
+
+    config = LoraConfig(
+        r=8,
+        lora_alpha=16,
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+            "lm_head",
+        ],
+        bias="none",
+        lora_dropout=0.05,  # Conventional
+        task_type="CAUSAL_LM",
+    )
+    model = get_peft_model(model, config)
+    print_trainable_parameters(model)
+    # Apply the accelerator. You can comment this out to remove the accelerator.
+    model = accelerator.prepare_model(model)
+
+    print(model)
+
+    if torch.cuda.device_count() > 1:  # If more than 1 GPU
+        model.is_parallelizable = True
+        model.model_parallel = True
+
+    project = "viggo-finetune"
+    base_model_name = "mistral"
+    run_name = base_model_name + "-" + project
+    output_dir = "./" + run_name
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=tokenized_train_dataset,
+        eval_dataset=tokenized_val_dataset,
+        args=transformers.TrainingArguments(
+            output_dir=output_dir,
+            warmup_steps=5,
+            per_device_train_batch_size=2,
+            gradient_accumulation_steps=4,
+            max_steps=1000,
+            learning_rate=2.5e-5,  # Want about 10x smaller than the Mistral learning rate
+            logging_steps=50,
+            bf16=True,
+            optim="paged_adamw_8bit",
+            logging_dir="./logs",  # Directory for storing logs
+            save_strategy="steps",  # Save the model checkpoint every logging step
+            save_steps=50,  # Save checkpoints every 50 steps
+            evaluation_strategy="steps",  # Evaluate the model every logging step
+            eval_steps=50,  # Evaluate and save checkpoints every 50 steps
+            do_eval=True,  # Perform evaluation at the end of training
+            report_to="wandb",  # Comment this out if you don't want to use weights & baises
+            run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}",  # Name of the W&B run (optional)
+        ),
+        data_collator=transformers.DataCollatorForLanguageModeling(
+            tokenizer, mlm=False
+        ),
+    )
+    model.config.use_cache = (
+        False  # silence the warnings. Please re-enable for inference!
+    )
+    trainer.train()
+
+    base_model = AutoModelForCausalLM.from_pretrained(
+        base_model_id,  # Mistral, same as before
+        quantization_config=bnb_config,  # Same quantization config as before
+        device_map="auto",
+        trust_remote_code=True,
+        use_auth_token=True,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(base_model_id, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    ft_model = PeftModel.from_pretrained(
+        base_model, "mistral-viggo-finetune/checkpoint-1000"
+    )
+
+    ft_model.eval()
+
+    model_input = tokenizer(prompt(dataset["test"][0]), return_tensors="pt").to("cuda")
+
+    with torch.no_grad():
+        print(
+            tokenizer.decode(
+                ft_model.generate(**model_input, max_new_tokens=100, pad_token_id=2)[0],
                 skip_special_tokens=True,
             )
         )
