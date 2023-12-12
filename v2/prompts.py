a63193819f385315ce59f9743b0595a1dbc66760
Erik <eekihenriksson@gmail.com>
2023-12-12 15:18:21 +0200

    Update prompts.py

--- v2/prompts.py
+++ v2/prompts.py
@@ -10,7 +10,7 @@ def run():
 
     base_model_id = "mistralai/Mistral-7B-v0.1"
     bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
+        # load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.bfloat16,
