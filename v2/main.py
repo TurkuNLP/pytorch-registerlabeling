0deb3f8b75891d1e1747874d6f8620f19e15a1bc
Erik <eekihenriksson@gmail.com>
2023-12-16 16:12:19 +0200

    Update main.py

--- v2/main.py
+++ v2/main.py
@@ -338,8 +338,8 @@ def run(options):
             )
 
             # add LoRA adaptor
-            model.gradient_checkpointing_enable()
-            model.config.use_cache = False
+            # model.gradient_checkpointing_enable()
+            # model.config.use_cache = False
 
             model = prepare_model_for_kbit_training(model)
             model = get_peft_model(model, lora_config)
