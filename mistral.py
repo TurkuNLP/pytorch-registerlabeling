09bfbc791ba92e7cc89175c5fb391b05d1ab4cf4
Erik <eekihenriksson@gmail.com>
2023-12-19 09:56:12 +0200

    Update mistral.py

--- mistral.py
+++ mistral.py
@@ -9,7 +9,7 @@ os.environ["XDG_CACHE_HOME"] = ".hf/xdg_cache_home"
 parser = ArgumentParser()
 parser.add_argument("--mode", "-m", default="train")
 parser.add_argument("--base_model", "-b", default="mistralai/Mistral-7B-Instruct-v0.2")
-parser.add_argument("--new_model", "-b", default="mistralai-finetuned")
+parser.add_argument("--new_model", "-n", default="mistralai-finetuned")
 parser.add_argument("--peft_model_path", "-p", default="")
 options = parser.parse_args()
 
