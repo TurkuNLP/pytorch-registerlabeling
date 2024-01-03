ac45ceec32766417c09cebece739ea3fd7b0a91e
Erik <eekihenriksson@gmail.com>
2024-01-03 16:36:14 +0200

    -gemini

--- v2/main.py
+++ v2/main.py
@@ -37,7 +37,7 @@ from .dataloader import (
 )
 from .modes.extract_embeddings import extract_doc_embeddings
 from .modes.extract_keywords import extract_doc_keywords
-from .utils import log_gpu_memory, infer_device_map, CustomModel
+from .utils import log_gpu_memory, infer_device_map
 
 current_optimal_threshold = 0.5  # Used in hierarchical loss (now obsolete)
 
@@ -355,14 +355,7 @@ def run(options):
             )
         if options.ignore_mismatched_sizes:
             params["ignore_mismatched_sizes"] = True
-
-        # Load the model
-
-        if options.gemini_embeddings:
-            print("Using Gemini embeddings")
-            model = CustomModel(768, 256, len(label_scheme))
-        else:
-            model = model_cls.from_pretrained(model_name, **params)
+        model = model_cls.from_pretrained(model_name, **params)
 
         if options.set_pad_id:
             model.config.pad_token_id = model.config.eos_token_id
@@ -463,9 +456,7 @@ def run(options):
         compute_metrics=compute_metrics,
         data_collator=DataCollatorWithPadding(
             tokenizer=tokenizer, padding="longest", max_length=options.max_length
-        )
-        if not options.gemini_embeddings
-        else None,
+        ),
         callbacks=[EarlyStoppingCallback(early_stopping_patience=options.patience)],
     )
 
