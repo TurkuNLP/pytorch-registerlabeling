332cd0bfe7875c694b28f8b3d7fa3990e0b4bcbd
Erik <eekihenriksson@gmail.com>
2024-01-11 21:26:41 +0200

    Update utils.py

--- v2/utils.py
+++ v2/utils.py
@@ -46,5 +46,5 @@ def preprocess_logits_for_metrics(logits, labels):
     Original Trainer may have a memory leak.
     This is a workaround to avoid storing too many tensors that are not needed.
     """
-    pred_ids = torch.argmax(logits, dim=-1)
-    return pred_ids, labels
+
+    return logits[0], labels
