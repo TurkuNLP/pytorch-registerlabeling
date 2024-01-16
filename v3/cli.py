2aed20b68032aa7f38145b2c491f58fc62833f67
Erik <eekihenriksson@gmail.com>
2024-01-16 13:46:46 +0200

    del unnecessary files

--- v3/cli.py
+++
@@ -1,9 +0,0 @@
-from jsonargparse import ArgumentParser
-
-
-def main_cli():
-    parser = ArgumentParser()
-    parser.add_argument("--opt2", type=float, default=1.0, help="Help for option 2.")
-    cfg = parser.parse_args()
-
-    return cfg
