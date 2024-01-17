8776da13c23cbe2d99031a6ba1b0d5b16979d17b
Erik <eekihenriksson@gmail.com>
2024-01-17 23:51:44 +0200

    Update main.py

--- v3/main.py
+++ v3/main.py
@@ -67,9 +67,6 @@ class Main:
             self.tokenizer.pad_token_id,
         )
 
-        # Init model
-        self._init_model()
-
         # Run
         getattr(self, cfg.method)()
 
@@ -179,6 +176,9 @@ class Main:
             project=self.cfg.working_dir.split("/", 1)[1].replace("/", ","),
             config=self.cfg,
         )
+
+        # Init model
+        self._init_model()
 
         num_training_steps = self.cfg.trainer.epochs * len(self.dataloaders["train"])
 
