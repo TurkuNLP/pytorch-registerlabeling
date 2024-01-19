29ea350e3c2f3a5a577a50c6b1c79c21065ac587
Erik <eekihenriksson@gmail.com>
2024-01-19 11:11:00 +0200

    Update main.py

--- v3/main.py
+++ v3/main.py
@@ -183,11 +183,9 @@ class Main:
         model = AutoModelForSequenceClassification.from_pretrained(
             self.cfg.model.name if not model_path else model_path,
             num_labels=self.cfg.num_labels,
-        )
+        ).to(dtype=self.cfg.torch_dtype)
 
         model = DataParallel(model)
-
-        model = model.to(self.cfg.device, dtype=self.cfg.torch_dtype)
 
         if self.cfg.model.compile:
             model = torch.compile(model)
