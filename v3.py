334f2cd8b2a2681a3d39791102726f95c7480113
Erik <eekihenriksson@gmail.com>
2024-01-28 19:36:10 +0200

    Update v3.py

--- v3.py
+++ v3.py
@@ -102,6 +102,7 @@ if __name__ == "__main__":
     parser.add_argument("--gpus", type=int, default=1)
     parser.add_argument("--resume", type=str)
     parser.add_argument("--no_tf32", action="store_true")
+    parser.add_argument("--bf16", action="store_true")
     parser.add_argument("--accelerate", action="store_true")
     parser.add_argument("--accelerate_bf16", action="store_true")
     parser.add_argument("--root_path", default="/scratch/project_2009199")
