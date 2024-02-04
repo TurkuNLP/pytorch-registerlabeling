974a2b0804f537ea9171b090f5e4ba3c0a4502c6
Erik <eekihenriksson@gmail.com>
2024-02-05 00:51:23 +0200

    setfit

--- v3/main.py
+++ v3/main.py
@@ -566,4 +566,4 @@ class Main:
         extract_st_doc_embeddings(self.model, self.dataset, path)
 
     def setfit_train(self):
-        setfit_train(self.dataset)
+        setfit_train(self.dataset, self.cfg.label_scheme)
--- v3/setfit_trainer.py
+++ v3/setfit_trainer.py
@@ -1,29 +1,61 @@
-from sentence_transformers.losses import CosineSimilarityLoss
 from setfit import SetFitModel, Trainer, TrainingArguments, sample_dataset
+
+from sklearn.metrics import (
+    accuracy_score,
+    average_precision_score,
+    classification_report,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+)
 
 model_id = "sentence-transformers/distiluse-base-multilingual-cased-v2"
 
 
-def setfit_train(dataset):
+def setfit_train(dataset, label_scheme):
+
+    def compute_metrics(y_pred, labels):
+        try:
+            roc_auc = roc_auc_score(labels, y_pred, average="micro")
+        except:
+            roc_auc = 0
+
+        precision = precision_score(labels, y_pred, average="micro")
+        recall = recall_score(labels, y_pred, average="micro")
+
+        try:
+            pr_auc = average_precision_score(labels, y_pred, average="micro")
+        except:
+            pr_auc = 0
+        accuracy = accuracy_score(labels, y_pred)
+        print(
+            classification_report(labels, y_pred, target_names=label_scheme, digits=4)
+        )
+        return {
+            f"f1": f1_score(y_true=labels, y_pred=y_pred, average="micro"),
+            f"precision": precision,
+            f"recall": recall,
+            f"pr_auc": pr_auc,
+            f"roc_auc": roc_auc,
+            f"accuracy": accuracy,
+        }
 
     train_dataset = sample_dataset(
-        dataset["train"], label_column="label_text", num_samples=1
+        dataset["train"], label_column="label_text", num_samples=16
     )
-    dev_dataset = sample_dataset(
-        dataset["dev"], label_column="label_text", num_samples=1
-    )
-    test_dataset = sample_dataset(
-        dataset["test"], label_column="label_text", num_samples=1
-    )
+    dev_dataset = dataset["dev"]
+
+    test_dataset = dataset["test"]
 
     model = SetFitModel.from_pretrained(model_id, multi_target_strategy="multi-output")
 
     args = TrainingArguments(
-        batch_size=64,
-        num_epochs=1,
+        batch_size=16,
+        num_epochs=5,
         evaluation_strategy="epoch",
         save_strategy="epoch",
-        load_best_model_at_end=True,
+        # load_best_model_at_end=True,
         report_to="none",
     )
 
@@ -32,7 +64,7 @@ def setfit_train(dataset):
         train_dataset=train_dataset,
         eval_dataset=dev_dataset,
         args=args,
-        metric="accuracy",
+        metric=compute_metrics,
         column_mapping={"text": "text", "labels": "label"},
     )
 
