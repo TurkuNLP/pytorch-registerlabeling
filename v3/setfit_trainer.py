d76ef03ad42b1f88ef0722ae8c2651f9c9d57da8
Erik <eekihenriksson@gmail.com>
2024-02-05 00:25:25 +0200

    Update setfit_trainer.py

--- v3/setfit_trainer.py
+++ v3/setfit_trainer.py
@@ -7,13 +7,13 @@ model_id = "sentence-transformers/distiluse-base-multilingual-cased-v2"
 def setfit_train(dataset):
 
     train_dataset = sample_dataset(
-        dataset["train"], label_column="label_text", num_samples=4
+        dataset["train"], label_column="label_text", num_samples=1
     )
     dev_dataset = sample_dataset(
-        dataset["dev"], label_column="label_text", num_samples=4
+        dataset["dev"], label_column="label_text", num_samples=1
     )
     test_dataset = sample_dataset(
-        dataset["test"], label_column="label_text", num_samples=4
+        dataset["test"], label_column="label_text", num_samples=1
     )
 
     model = SetFitModel.from_pretrained(model_id, multi_target_strategy="multi-output")
