5cfffdf1ea34ff425cce6474546c3a5ea6541c7c
Erik <eekihenriksson@gmail.com>
2024-02-04 23:15:00 +0200

    Update setfit_trainer.py

--- v3/setfit_trainer.py
+++ v3/setfit_trainer.py
@@ -13,7 +13,7 @@ def setfit_train(dataset):
         evaluation_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
-        report_to=None,
+        report_to="none",
     )
 
     trainer = Trainer(
