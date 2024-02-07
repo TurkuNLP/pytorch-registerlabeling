3f2de69ea17ed99d000aa1747bf0cdbfc05a4dd7
Erik <eekihenriksson@gmail.com>
2024-02-07 23:08:33 +0200

    Update setfit_trainer.py

--- v3/setfit_trainer.py
+++ v3/setfit_trainer.py
@@ -78,7 +78,7 @@ def setfit_train(dataset, label_scheme):
     )
 
     args = TrainingArguments(
-        # batch_size=16,
+        batch_size=8,
         evaluation_strategy="steps",
         eval_steps=500,
         num_epochs=1,
