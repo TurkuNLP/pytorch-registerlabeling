311bdb5de757d995216b03b299a69bfe29b6f4b4
Erik <eekihenriksson@gmail.com>
2024-02-19 00:22:48 +0200

    Update setfit_trainer.py

--- v3/setfit_trainer.py
+++ v3/setfit_trainer.py
@@ -66,7 +66,7 @@ def setfit_train(dataset, label_scheme, method):
         }
 
     if method == "predict":
-        model_id = "./checkpoints/step_1000"
+        model_id = "./setfit"
     else:
         model_id = "smart-tribune/sentence-transformers-multilingual-e5-large"
 
