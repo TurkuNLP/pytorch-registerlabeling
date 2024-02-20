ba922031e530cad0eda6f2cfed7b611dedd6ff11
Erik <eekihenriksson@gmail.com>
2024-02-20 19:55:03 +0200

    Update v3.sh

--- v3.sh
+++ v3.sh
@@ -11,7 +11,7 @@ if [[ -z "$SLURM_JOB_ID" ]]; then
   DYNAMIC_JOBNAME="$1"
   shift  # Shift command-line arguments to remove the first one
   # Submit the job to slurm with the dynamic job name and capture the output
-  JOB_SUBMISSION_OUTPUT=$(sbatch --job-name="$DYNAMIC_JOBNAME" -o "logs/${DYNAMIC_JOBNAME}-%j.log" "$0" -- "$@")
+  JOB_SUBMISSION_OUTPUT=$(sbatch --job-name="$DYNAMIC_JOBNAME" -o "logs/${DYNAMIC_JOBNAME}-%j.log" "$0" "$@")
   echo "Submission output: $JOB_SUBMISSION_OUTPUT"
   # Extract the job ID from the submission output
   JOB_ID=$(echo "$JOB_SUBMISSION_OUTPUT" | grep -oP 'Submitted batch job \K\d+')
@@ -24,5 +24,6 @@ if [[ -z "$SLURM_JOB_ID" ]]; then
 else
   # Actual job script starts here
   source venv/bin/activate
+  shift
   python3 v3.py "$@"
 fi
