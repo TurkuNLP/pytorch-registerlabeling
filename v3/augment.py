f4c9402ae397ec64427c71ae0aa477916e52229f
Erik <eekihenriksson@gmail.com>
2024-02-25 10:59:35 +0200

    updates

--- v3/augment.py
+++ v3/augment.py
@@ -27,41 +27,51 @@ class Augment:
             f"Helsinki-NLP/opus-mt-{self.cfg.target}-{self.cfg.source}"
         )
 
-        with open(
-            f"data/{self.cfg.source}/train_aug.tsv", "w", encoding="utf-8"
-        ) as outfile:
+        def iterate_in_batches(lst, batch_size):
+            for i in range(0, len(lst), batch_size):
+                yield lst[i : i + batch_size]
 
-            with open(f"data/{self.cfg.source}/train.tsv", "r") as f:
-                # Parse the tsv file into a list of lists
-                rows = [line.strip().split("\t") for line in f]
+        # with open(
+        #    f"data/{self.cfg.source}/train_aug.tsv", "w", encoding="utf-8"
+        # ) as outfile:
 
-                for row in rows:
+        with open(f"data/{self.cfg.source}/train.tsv", "r") as f:
+            # Parse the tsv file into a list of lists
+            rows = [line.strip().split("\t") for line in f]
+            rows = [row for row in rows if len(row) > 1 and row[0] and row[1]]
 
-                    if not len(row) > 1 and row[0] and row[1]:
-                        continue
+            for batch in iterate_in_batches(rows, 8):
 
-                    src_text = " ".join(row[1].split()[:300])
+                batch = [[x[0], " ".join(x[1].split()[:300])] for x in batch]
 
-                    translated = source_tokenizer.decode(
-                        source_model.generate(
-                            **source_tokenizer(
-                                src_text,
-                                return_tensors="pt",
-                                padding=True,
-                            )
-                        )[0],
+                translated_tokens = (
+                    source_model.generate(
+                        **source_tokenizer(
+                            batch,
+                            return_tensors="pt",
+                            padding=True,
+                        )
+                    ),
+                )
+
+                translated = [
+                    source_tokenizer.decode(
+                        t,
                         skip_special_tokens=True,
                     )
-                    print(translated)
-                    back_translated = target_tokenizer.decode(
-                        target_model.generate(
-                            **target_tokenizer(
-                                translated,
-                                return_tensors="pt",
-                                padding=True,
-                            )
-                        )[0],
-                        skip_special_tokens=True,
-                    )
+                    for t in translated_tokens
+                ]
+                print(translated)
+                exit()
+                back_translated = target_tokenizer.decode(
+                    target_model.generate(
+                        **target_tokenizer(
+                            translated,
+                            return_tensors="pt",
+                            padding=True,
+                        )
+                    )[0],
+                    skip_special_tokens=True,
+                )
 
-                    outfile.write(f"{row[0]}\t{back_translated}\n")
+                outfile.write(f"{row[0]}\t{back_translated}\n")
