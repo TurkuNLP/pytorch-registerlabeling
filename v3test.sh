deb01d73e33c4398d971df78a15ba5d8a71b1178
Erik <eekihenriksson@gmail.com>
2024-02-26 13:35:16 +0200

    Create v3test.sh

---
+++ v3test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=gputest
+#SBATCH --time=0:15:00
+#SBATCH --account=project_2009199
+#SBATCH --mail-user=pytorchregisterlabeling@gmail.com 
+#SBATCH --mail-type=ALL
+
+if [[ -z "$SLURM_JOB_ID" ]]; then
+  DYNAMIC_JOBNAME="$1"
+  shift  # Shift command-line arguments to remove the first one
+  # Submit the job to slurm with the dynamic job name and capture the output
+  JOB_SUBMISSION_OUTPUT=$(sbatch --job-name="$DYNAMIC_JOBNAME" -o "logs/${DYNAMIC_JOBNAME}-%j.log" "$0" "$@")
+  echo "Submission output: $JOB_SUBMISSION_OUTPUT"
+  # Extract the job ID from the submission output
+  JOB_ID=$(echo "$JOB_SUBMISSION_OUTPUT" | grep -oP 'Submitted batch job \K\d+')
+  LOG_FILE="logs/${DYNAMIC_JOBNAME}-${JOB_ID}.log"
+  touch $LOG_FILE
+  echo "Tailing log file: $LOG_FILE"
+  # Use tail -f to follow the log file
+  tail -f "$LOG_FILE"
+  exit $?
+else
+  # Actual job script starts here
+  source venv/bin/activate
+  srun python3 "$@"
+fi
