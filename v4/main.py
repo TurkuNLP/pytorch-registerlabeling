ee4d279cdc24bee62e36475a3e737e05be5bc7a9
Erik <eekihenriksson@gmail.com>
2024-03-19 09:19:36 +0200

    Update main.py

--- v4/main.py
+++ v4/main.py
@@ -36,7 +36,7 @@ def run(cfg):
 
     tokenizer = AutoTokenizer.from_pretrained(cfg.model_name)
     labels = label_schemes[cfg.labels]
-    output_dir = f"{cfg.root}/hf_output/{cfg.model_name}{("_"+cfg.path_suffix) if cfg.path_suffix else ""}/labels_{cfg.labels}/{cfg.train}_{cfg.test}/seed_{cfg.seed}"
+    output_dir = f"{cfg.root}/hf_output/{cfg.model_name}{('_'+cfg.path_suffix) if cfg.path_suffix else ''}/labels_{cfg.labels}/{cfg.train}_{cfg.test}/seed_{cfg.seed}"
     model_path = output_dir if cfg.method == "test" else cfg.model_name
     dataset = get_dataset(cfg, tokenizer)
 
