51323f60426fc780aacbefcb55a4da6825e996de
Erik <eekihenriksson@gmail.com>
2024-03-25 10:59:25 +0200

    updates

--- v4/main.py
+++ v4/main.py
@@ -198,6 +198,7 @@ def run(cfg):
         train_dataset=dataset.get("train", []),
         eval_dataset=dataset.get("dev", []),
         compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
     )
 
     if cfg.method == "train":
