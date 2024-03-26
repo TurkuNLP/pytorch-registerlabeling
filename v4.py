5f636761566ff7ffd67a05233d2656b858ce8534
Erik <eekihenriksson@gmail.com>
2024-03-26 14:44:00 +0200

    Update v4.py

--- v4.py
+++ v4.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     parser.add_argument("--labels", default="all")
 
     # Trainer
-    parser.add_argument("--learning_rate", "-lr", type=float, default=3e-5)
+    parser.add_argument("--learning_rate", "-lr", type=float, default=5e-5)
     parser.add_argument("--train_batch_size", "-bt", type=int, default=8)
     parser.add_argument("--eval_batch_size", "-bd", type=int, default=8)
     parser.add_argument("--max_length", type=int, default=512)
