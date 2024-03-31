afc48408f80bec73dbe61412f76736c43260e0e4
Erik <eekihenriksson@gmail.com>
2024-03-31 14:35:02 +0300

    Update train.py

--- v4/train.py
+++ v4/train.py
@@ -176,7 +176,7 @@ def run(cfg):
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_use_double_quant=True,
-        # bnb_4bit_compute_dtype=torch_dtype,
+        bnb_4bit_compute_dtype=torch_dtype,
     )
 
     model = AutoModelForSequenceClassification.from_pretrained(
